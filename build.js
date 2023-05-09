944767ae44ee6596ae99e69d5d53f4f8efb88e63
Caoqizhi <2391580279@qq.com>
2023-05-09 15:21:17 +0800

    en: 完成脚本拉取docs

---
+++ build.js
@@ -0,0 +1,120 @@
+require('dotenv').config();
+const https = require('https');
+const fs = require('fs');
+const fsextra = require('fs-extra');
+const path = require('path');
+const { exec } = require('child_process');
+
+const filesToDownload = JSON.parse(process.env.FILES_TO_DOWNLOAD).map(e => {
+    const url = e.split("/").reverse();
+    return e = `https://codeload.github.com/${url[1]}/${url[0]}/zip/refs/heads/main`
+})
+
+const filesNames = JSON.parse(process.env.FILES_TO_DOWNLOAD).map(e => {
+    const url = e.split("/").reverse();
+    return e = url[0]
+})
+
+const downloadFile = (url, destinationPath) => {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      const stream = fs.createWriteStream(destinationPath);
+      res.pipe(stream);
+      stream.on('finish', () => {
+        console.log(`File downloaded successfully: ${destinationPath}`);
+        resolve();
+      });
+    }).on('error', (err) => {
+      console.error(`Error downloading file: ${err.message}`);
+      reject(err);
+    });
+  });
+};
+
+const downloadAllFiles = async (folder) => {
+  for (let i = 0; i < filesToDownload.length; i++) {
+    const file = filesToDownload[i];
+    await downloadFile(file, folder+"/"+i+".zip");
+  }
+};
+
+const extractFiles = (sourcePath, destinationPath) => {
+  return new Promise((resolve, reject) => {
+    const extractCommand = `unzip -q -n ${sourcePath} -d ${destinationPath}`;
+    exec(extractCommand, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error extracting file: ${err}`);
+        reject(err);
+      } else {
+        console.log(`Files extracted successfully: ${destinationPath}`);
+        resolve();
+      }
+    });
+  });
+};
+
+const extractFilesAndCopyFolder = async(destinationPath) => {
+    for (let i = 0; i < filesToDownload.length; i++) {
+        const sourcePath = destinationPath+"/"+i+".zip"
+        try {
+            // Extract files from sourcePath to destinationPath
+            await extractFiles(sourcePath, destinationPath);
+            // 将 folderToCopy 从 destinationPath 复制到另一个文件夹
+            // TODO ===> "unzip"+i to ==> xx-main
+            const sourceFolder = path.join(destinationPath, filesNames[i]+"-main/docs");
+            const destinationFolder = `./docs/${filesNames[i]}`;
+            await fsextra.copy(sourceFolder, destinationFolder);
+            console.log('Files extracted and folder copied successfully');
+            } catch (err) {
+            console.error(err);
+            }
+    }
+}
+
+const createFolder = (folderPath) => {
+    fs.mkdir(folderPath, (err) => {
+      if (err) {
+        console.error(`Error creating folder: ${err}`);
+      } else {
+        console.log(`Folder created successfully: ${folderPath}`);
+      }
+    });
+};
+  
+
+const buildProject = () => {
+  return new Promise((resolve, reject) => {
+    const buildCommand = 'npm run build';
+    exec(buildCommand, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error running build command: ${err}`);
+        reject(err);
+      } else {
+        console.log('Build completed successfully');
+        resolve();
+      }
+    });
+  });
+};
+
+const main = async () => {
+
+  // mkdir
+  const folder = "./tmpl";
+  createFolder(folder);
+
+  // Download all files
+  await downloadAllFiles(folder);
+
+  // Extract downloaded files
+  // copy to the docs
+  await extractFilesAndCopyFolder(folder);
+
+  // Delete destinationPath
+  await fsextra.remove(folder);
+
+  // Build project
+  await buildProject();
+};
+
+main();
