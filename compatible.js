e9b45620c94f6e4d351f1bbcb8cb70b46fd74299
Caoqizhi <2391580279@qq.com>
2023-06-15 14:51:03 +0800

    en: 完成<成为链上数据分析师>gitbook兼容

--- compatible.js
+++ compatible.js
@@ -5,6 +5,26 @@ const common = [
         path: "./docs/MasteringChainAnalytics/08_nft_analysis/readme.md",
         oldValue: "<br>",
         newValue: "<br/>"
+    },
+    {
+        path: "./docs/MasteringChainAnalytics/README.md",
+        oldValue: "# Mastering Chain Analytics",
+        newValue: "# 简介"
+    },
+    {
+        path: "./docs/MasteringChainAnalytics/00_introductions/readme.md",
+        oldValue: "# 成为链上数据分析师#0",
+        newValue: "# 成为链上数据分析师"
+    },
+    {
+        path: "./docs/MasteringChainAnalytics/01_platform/dune.md",
+        oldValue: "## Dune平台介绍",
+        newValue: `# Dune平台简介\n## Dune平台介绍`
+    },
+    {
+        path: "./docs/MasteringChainAnalytics/21_how_to_design_a_dashboard/readme.md",
+        oldValue: "# 如何设计一个Dshboard-以BTC指标CDD(Coin Day Destroyed)为例",
+        newValue: "# 如何设计Dashboard - 以BTC指标CDD为例"
     }
 ]
 
