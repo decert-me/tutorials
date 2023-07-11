4d892140e5183ae57f88636435f32c475b6545f5
Caoqizhi <2391580279@qq.com>
2023-07-11 16:49:19 +0800

    en: 初始化wagmi web3modal

---
+++ src/theme/Root.js
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { WagmiConfig, configureChains, createClient } from 'wagmi';
+import { mainnet, polygon, polygonMumbai } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+
+// Default implementation, that you can customize
+export default function Root({children}) {
+
+    const projectId = process.env.PROJECT_ID;
+
+    const { chains, provider, webSocketProvider } = configureChains(
+        [mainnet, polygonMumbai, polygon],
+        [publicProvider()],
+        [w3mProvider({ projectId })],
+        { targetQuorum: 1 },
+    )
+
+    const web3modalClient = createClient({
+        autoConnect: true,
+        connectors: [
+          ...w3mConnectors({ projectId, version: 2, chains })
+        ],
+        provider,
+        webSocketProvider,
+    })
+
+    const wagmiClient = createClient({
+        autoConnect: true,
+        connectors: [
+          new MetaMaskConnector({ chains }),
+          new WalletConnectConnector({
+            chains,
+            options: {
+              projectId: projectId,
+              showQrModal: true
+            },
+          }),
+        ],
+        provider,
+        webSocketProvider,
+    })
+
+    const ethereumClient = new EthereumClient(web3modalClient, chains)
+
+    useEffect(() => {
+        console.log(process.env.PROJECT_ID);
+    },[])
+
+    return (
+        <>
+            <WagmiConfig client={wagmiClient}>
+                <>{children}</>
+            </WagmiConfig>
+            <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+        </>
+    )
+    
+    
+}
