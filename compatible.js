fc885a1caf8bb6543dea002deccbba473ad54429
Caoqizhi <2391580279@qq.com>
2023-07-17 17:02:40 +0800

    en: 兼容mdBook

--- compatible.js
+++ compatible.js
@@ -25,6 +25,36 @@ const common = [
         path: "./docs/MasteringChainAnalytics/21_how_to_design_a_dashboard/readme.md",
         oldValue: "# 如何设计一个Dshboard-以BTC指标CDD(Coin Day Destroyed)为例",
         newValue: "# 如何设计Dashboard - 以BTC指标CDD为例"
+    },
+    {
+        path: "./docs/sui-move-intro-course-zh/advanced-topics/BCS_encoding/lessons/BCS_encoding.md",
+        oldValue: "../example_projects/",
+        newValue: "https://github.com/RandyPen/sui-move-intro-course-zh/tree/main/advanced-topics/BCS_encoding/example_projects"
+    },
+    {
+        path: "./docs/sui-move-intro-course-zh/unit-four/lessons/4_marketplace_contract.md",
+        oldValue: "../../unit-two/lessons/6_capability_design_pattern.md",
+        newValue: "../../unit-two/lessons/6_capability_设计模式.md"
+    },
+    {
+        path: "./docs/sui-move-intro-course-zh/advanced-topics/BCS_encoding/lessons/BCS_encoding.md",
+        oldValue: "../../unit-one/lessons/4_定制类型与能力.md#定制类型与能力",
+        newValue: "../../unit-one/lessons/4_函数.md"
+    },
+    {
+        path: "./docs/sui-move-intro-course-zh/advanced-topics/BCS_encoding/lessons/BCS_encoding.md",
+        oldValue: "../../unit-one/lessons/6_hello_world.md#viewing-the-object-with-sui-explorer",
+        newValue: "../../unit-one/lessons/5_hello_world.md"
+    },
+    {
+        path: "./docs/sui-move-intro-course-zh/advanced-topics/BCS_encoding/lessons/BCS_encoding.md",
+        oldValue: "../example_projects/",
+        newValue: "https://github.com/RandyPen/sui-move-intro-course-zh/tree/main/advanced-topics/BCS_encoding/example_projects"
+    },
+    {
+        path: "./docs/sui-move-intro-course-zh/advanced-topics/BCS_encoding/lessons/BCS_encoding.md",
+        oldValue: "../example_projects/",
+        newValue: "https://github.com/RandyPen/sui-move-intro-course-zh/tree/main/advanced-topics/BCS_encoding/example_projects"
     }
 ]
 
