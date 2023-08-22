ffd9a4280f2f0331f7e43a576ea8b8aa32074178
Caoqizhi <2391580279@qq.com>
2023-08-22 10:10:26 +0800

    en: 修改兼容处理

--- compatible.js
+++ compatible.js
@@ -5,34 +5,76 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 async function modifyFile() {
     const compatibility_table = require('./compatibility_table');
-    const { replace: arr, empty } = compatibility_table;
+    const tutorials_table = require('./tutorials.json');
+    const { replace: arr, empty, normal } = compatibility_table;
 
-    for (let i = 0; i < arr.length; i++) {
-      try {
-        // 读取文件
-        const data = await readFileAsync(arr[i].path, 'utf8');
-        
-        // 修改文件内容
-        const modifiedData = data.replace(arr[i].oldValue, arr[i].newValue);
-        
-        // 写入文件
-        await writeFileAsync(arr[i].path, modifiedData, 'utf8');
-        console.log(`File ${arr[i].path} modified successfully.`);
-      } catch (err) {
-        console.error(`Error modifying file ${arr[i].path}: ${err}`);
+    for (const key in arr) {
+      if (Object.hasOwnProperty.call(arr, key)) {
+        if (tutorials_table.filter(ele => ele.repoUrl === key).length === 0) {
+          continue;
+        }
+        const selectArr = arr[key];
+        const selectTutorial = tutorials_table.filter(ele => ele.repoUrl)[0];
+        const path = `./docs/${selectTutorial.catalogueName}`
+
+        for (let i = 0; i < selectArr.length; i++) {
+          const item = selectArr[i]
+          try {
+            // 读取文件
+            const data = await readFileAsync(path + item.path, 'utf8');
+            
+            // 修改文件内容
+            const modifiedData = data.replace(item.oldValue, item.newValue);
+            
+            // 写入文件
+            await writeFileAsync(path + item.path, modifiedData, 'utf8');
+            console.log(`File ${path + item.path} modified successfully.`);
+          } catch (err) {
+            console.error(`Error modifying file ${path + item.path}: ${err}`);
+          }
+        }
       }
     }
-    // 添加空文档处理
-    for (let i = 0; i < empty.length; i++) {
-        const element = empty[i];
-        try {
-            const fileData = element.newValue
-            await writeFileAsync(element.path, fileData, 'utf8');
-            console.log(`File ${element.path} modified successfully.`);
-        } catch (error) {
-            console.error(`Error modifying file ${element.path}: ${error}`);
-        }
+
+    for (let i = 0; i < normal.length; i++) {
+      try {
+        // 读取文件
+        const data = await readFileAsync(normal[i].path, 'utf8');
+        
+        // 修改文件内容
+        const modifiedData = data.replace(normal[i].oldValue, normal[i].newValue);
+        
+        // 写入文件
+        await writeFileAsync(normal[i].path, modifiedData, 'utf8');
+        // console.log(`File ${normal[i].path} modified successfully.`);
+      } catch (err) {
+        // console.error(`Error modifying file ${normal[i].path}: ${err}`);
+      }
     }
+    
+    // 添加空文档处理
+    for (const key in empty) {
+      if (Object.hasOwnProperty.call(empty, key)) {
+        if (tutorials_table.filter(ele => ele.repoUrl === key).length === 0) {
+          continue;
+        }
+        const selectArr = empty[key];
+        const selectTutorial = tutorials_table.filter(ele => ele.repoUrl)[0];
+        const path = `./docs/${selectTutorial.catalogueName}`
+
+        for (let i = 0; i < selectArr.length; i++) {
+          const element = selectArr[i];
+          try {
+              const fileData = element.newValue
+              await writeFileAsync(path + element.path, fileData, 'utf8');
+              // console.log(`File ${element.path} modified successfully.`);
+          } catch (error) {
+              // console.error(`Error modifying file ${element.path}: ${error}`);
+          }
+        }
+      }
+    }
+    
   }
 
 
